acd464144384cfe1dc0853e03f44c5cd8fd00268
Ravinder Singhmar <Ravinder.Singhmar@gmail.com>
2026-08-09 13:45:00 +0530

    Fixed Checkov skip comment placement

--- modules/azurerm_subnet/main.tf
+++ modules/azurerm_subnet/main.tf
@@ -1,5 +1,5 @@
-# checkov:skip=CKV2_AZURE_31:NSG is not required for this environment yet
 resource "azurerm_subnet" "dev-subnet" {
+  #checkov:skip=CKV2_AZURE_31:NSG is not required for this environment yet
   for_each             = var.subnetdetails
   name                 = each.value.name
   resource_group_name  = each.value.resource_group_name
