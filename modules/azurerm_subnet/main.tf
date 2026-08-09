e249516566d4fd9e8f77b0c44650874e3578480e
Ravinder Singhmar <Ravinder.Singhmar@gmail.com>
2026-08-09 13:39:44 +0530

    Skipped Checkov NSG rule temporarily

--- modules/azurerm_subnet/main.tf
+++ modules/azurerm_subnet/main.tf
@@ -1,8 +1,8 @@
+# checkov:skip=CKV2_AZURE_31:NSG is not required for this environment yet
 resource "azurerm_subnet" "dev-subnet" {
   for_each             = var.subnetdetails
   name                 = each.value.name
   resource_group_name  = each.value.resource_group_name
   virtual_network_name = each.value.virtual_network_name
   address_prefixes     = each.value.address_prefixes
-
 }
