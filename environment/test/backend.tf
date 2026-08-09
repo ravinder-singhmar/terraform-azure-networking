7d37b5b83922b79e9d704ae6e913d14d5bdd56ef
Ravinder Singhmar <Ravinder.Singhmar@gmail.com>
2026-08-09 14:06:42 +0530

    Added Azure RM Remote Backend and configured CD pipeline

---
+++ environment/test/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tfstate"
+    storage_account_name = "stgbackendtfstate"
+    container_name       = "tfstate-backend"
+    key                  = "test.tfstatefile"
+
+  }
+}
